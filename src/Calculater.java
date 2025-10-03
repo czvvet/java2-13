abef6ea162cf772b24d2401abccca34a218d35d9
czvvet <bsi25031@uhe.ac.jp>
2025-10-03 13:51:05 +0900

    足し算引き算掛け算割り算の追加

--- src/Calculater.java
+++ src/Calculater.java
@@ -1,10 +1,37 @@
 
 public class Calculater {
+	
+	//足し算
+	void add(double decimalAdd1,double decimalAdd2) {
+		double answerAdd = decimalAdd1 + decimalAdd2;
+		System.out.println(answerAdd);
+	}
+	
+	//引き算
+	void sub(double decimalSub1,double decimalSub2) {
+		double answerSub = decimalSub1 - decimalSub2;
+		System.out.println(answerSub);
+	}
+	
+	//かけ算
+	void mul(double decimalMul1,double decimalMul2) {
+		double answerMul = decimalMul1 * decimalMul2;
+		System.out.println(answerMul);
+	}
+	
+	//割り算
+	void div(double decimalDiv1,double decimalDiv2) {
+		double answerDiv = decimalDiv1 / decimalDiv2;
+		System.out.println(answerDiv);
+	}
+	
+	//平方根
 	void squareNumber(double decimal) {
 		double answer = decimal * decimal;
 		System.out.println(answer);
 	}
 	
+	//階乗
 	void factorial(int number) {
 		int answer = 1;
 		
--- src/CalculaterApp.java
+++ src/CalculaterApp.java
@@ -5,27 +5,63 @@ public class CalculaterApp {
 		Calculater caic = new Calculater();
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("平方根を求めるか階乗を求めるか選択してください");
+		System.out.println("1~6の中から選択してください");
+		System.out.println("1:足し算");
+		System.out.println("2:引き算");
+		System.out.println("3:掛け算");
+		System.out.println("4:割り算");
+		System.out.println("5:平方根");
+		System.out.println("6:階乗");
 		String num1 = scanner.nextLine();
 		
-		if(num1.equals("平方根")) {
+		if(num1.equals("1")) {
+			
+			System.out.println("1つ目の数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("2つ目の数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			caic.add(argument1,argument2);
+			
+		}else if(num1.equals("2")) {
+			
+			System.out.println("1つ目の数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("2つ目の数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			caic.sub(argument1,argument2);
+			
+		}else if(num1.equals("3")){
+			
+			System.out.println("1つ目の数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("2つ目の数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			caic.mul(argument1,argument2);
+			
+		}else if(num1.equals("4")) {
+			
+			System.out.println("1つ目の数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("2つ目の数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			caic.div(argument1,argument2);
+			
+		}else if(num1.equals("5")) {
 			
 			System.out.println("数字をひとつ入力してください。");
 			double argument = scanner.nextDouble();
-			
 			caic.squareNumber(argument);
 			
-		}else if(num1.equals("階乗")) {
+		}else if(num1.equals("6")) {
 			
 			System.out.println("整数をひとつ入力してください。");
 			int num = scanner.nextInt();
-			
 			caic.factorial(num);
 			
 		}else {
-			System.out.println("平方根か階乗かで選択してください");
+			System.out.println("1~6の中から選択してください");
 		}
-			
+		
 		scanner.close();
 	}
 
